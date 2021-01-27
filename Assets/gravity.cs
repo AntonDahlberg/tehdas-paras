266e4304167baf263611de533c65ac2c6d480f5d
AntonDahlberg <58072626+AntonDahlberg@users.noreply.github.com>
2021-01-27 12:12:58 +0200

    zombi

---
+++ Assets/gravity.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gravity : MonoBehaviour
+{
+    public float Gravity = -20f;
+    Vector3 velocity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        velocity.y = Mathf.Sqrt(Gravity);
+    }
+}
