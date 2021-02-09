3336cde2d3db88b0d2eb2f923af44f7460d98b5a
AntonDahlberg <58072626+AntonDahlberg@users.noreply.github.com>
2021-02-09 11:45:53 +0200

    knife

---
+++ Assets/Damaged.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damaged : MonoBehaviour
+{
+  
+    public int damage;
+    enemy Enemyhp;
+
+    // Start is called before the first frame update
+
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+
+
+
+
+
+
+    public void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+            Enemyhp = other.transform.GetComponent<enemy>();
+            Enemyhp.Health -= damage;
+            
+
+
+
+        }
+
+    }
+
+}
