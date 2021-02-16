f06170ac3ed2e36d2c29916e4c2336bbb2afd47f
AntonDahlberg <58072626+AntonDahlberg@users.noreply.github.com>
2021-02-16 13:08:52 +0200

    aaa

--- Assets/follow.cs
+++ Assets/follow.cs
@@ -5,19 +5,19 @@ using UnityEngine;
 public class follow : MonoBehaviour
 {
     public Transform target;
-    public float speed;
-    Rigidbody rig;
+    public Transform enemy;
+   
+    
     // Start is called before the first frame update
     void Start()
     {
-        rig = GetComponent<Rigidbody>(); 
+        
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        Vector3 pos = Vector3.MoveTowards(transform.position, target.position, speed * Time.fixedDeltaTime);
-        rig.MovePosition(pos);
-        transform.LookAt(target);
+
+        enemy.LookAt(target.transform);
     }
 }
