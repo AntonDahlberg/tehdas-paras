2f8ebc39b7e007e7a99db0a73c78686eeef34361
AntonDahlberg <58072626+AntonDahlberg@users.noreply.github.com>
2021-02-17 13:48:06 +0200

    tädää

---
+++ Assets/tekstiaa.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class tekstiaa : MonoBehaviour
+{
+    public Text m_MyText;
+    public GameObject tekstini;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(ExampleCoroutine());
+        m_MyText.text = "open door and get key and kill enemis and open key wall you win.";
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    IEnumerator ExampleCoroutine()
+    {
+        yield return new WaitForSeconds(15);
+        tekstini.gameObject.SetActive(false);
+    }
+}
