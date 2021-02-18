1765b3bb351f1611c0d92a0fe5da9623dfcaf92f
AntonDahlberg <58072626+AntonDahlberg@users.noreply.github.com>
2021-02-18 14:13:15 +0200

    aaa

--- Assets/end.cs
+++ Assets/end.cs
@@ -5,13 +5,15 @@ using UnityEngine;
 public class end : MonoBehaviour
 {
     public GameObject endgame;
+    public GameObject aika;
     // Start is called before the first frame update
-    
+
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.tag == "Player")
         {
             endgame.gameObject.SetActive(true);
+            aika.gameObject.SetActive(false);
             Destroy(gameObject);
             Cursor.lockState = CursorLockMode.None;
             
